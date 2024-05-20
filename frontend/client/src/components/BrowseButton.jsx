bbe6398ca3e2d252ea55a3b8218e25f1307bf240
MABURIKU <MaburikuGraphics@gmail.com>
2024-05-21 00:35:25 +0800

    style: General Styling (Home.jsx, Navigation.jsx, Footer.jsx)

--- frontend/client/src/components/BrowseButton.jsx
+++ frontend/client/src/components/BrowseButton.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-function BuildButton() {
+function BrowseButton() {
     return (
         <>
-            <NavLink to="/Build">
+            <NavLink to="/Catalog">
                 <div className="group text-center w-[100%] lg:w-[300px] px-[50px] py-[10px] border-[3px] border-QKGreen hover:bg-QKGreen rounded-full transition-all duration-200">
                     <p className="text-[20px] lg:text-[25px] text-QKGreen group-hover:text-BGMain">
-                        Start Building
+                        Start Browsing
                     </p>
                 </div>
             </NavLink>
@@ -15,4 +15,4 @@ function BuildButton() {
     )
 }
 
-export default BuildButton
+export default BrowseButton
